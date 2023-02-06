175821cb2bcbba525090d332969204d822c14019
apollo110217 <robertosa256453@gmail.com>
2023-02-06 12:00:53 -0800

    feat: add file

---
+++ src/App2.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import ConnectWallet from './components/connet-wallet'
+
+function App() {
+  return (
+    <div className="App">
+      <ConnectWallet />
+    </div>
+  );
+}
+
+export default App;
